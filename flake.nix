83e042384b20c90a8a0172826ace07654111fc2b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-13 14:11:45 +0100

    chore: create sd card build command for nixos

--- flake.nix
+++ flake.nix
@@ -5,16 +5,18 @@
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }:
-    {
-      nixosConfigurations = {
-        cyberfish = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            nixos-hardware.nixosModules.raspberry-pi-3
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-            ./configuration.nix
-          ];
-        };
+  {
+    nixosConfigurations = {
+      cyberfish = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-hardware.nixosModules.raspberry-pi-3
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+          ./configuration.nix
+        ];
       };
     };
+    # Set default build to create SD image
+    packages.aarch64-linux.default = self.nixosConfigurations.cyberfish.config.system.build.sdImage;
+  };
 }
