451d261a263753deb90081dc628873dcbba203d7
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-13 14:55:46 +0100

    Reapply "chore: try removing interval"
    This reverts commit 19b12cb160ca20602ba8d0810eb07150f6e39006.

--- configuration.nix
+++ configuration.nix
@@ -7,11 +7,6 @@
   nix = {
     gc = {
       automatic = true;
-      interval = {
-        Hour = 3;
-        Minute = 15;
-        Weekday = 7;
-      };
       options = "--delete-older-than 30d";
     };
     settings = {
