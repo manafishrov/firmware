17d9f3bd424263e86863f48cb7da860fe7e0f229
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-24 17:42:14 +0200

    feat: first working nixos image

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         system = "aarch64-linux";
         modules = [
           nixos-hardware.nixosModules.raspberry-pi-3
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
           ./configuration.nix
         ];
       };
