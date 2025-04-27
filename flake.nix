782b6ed8dc06005b80e7363349c2f2f378d3eb6d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-28 00:18:50 +0200

    chore: try new flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11?shallow=1";
     nixos-hardware.url = "github:NixOS/nixos-hardware?shallow=1";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi?shallow=1";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }:
+  outputs = { self, nixpkgs, nixos-hardware, nixos-raspberrypi, ... }:
   {
     nixosConfigurations = {
-      cyberfish = nixpkgs.lib.nixosSystem {
+      cyberfish = nixos-raspberrypi.lib.nixosSystemFull {
+        specialArgs = { inherit nixos-raspberrypi; };
         system = "aarch64-linux";
-        specialArgs = { inherit nixos-hardware; };
         modules = [
-          nixos-hardware.nixosModules.raspberry-pi-3
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-          ./camera.nix
+          nixos-hardware.nixosModules.raspberry-pi-3
           ./configuration.nix
         ];
       };
     };
-    # Build to create SD image
     packages.aarch64-linux.sdImage = self.nixosConfigurations.cyberfish.config.system.build.sdImage;
   };
 }
