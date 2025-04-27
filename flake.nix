6ed25d3edf59fba6a638c4d7422eb9a047dd02c8
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-04-28 00:21:29 +0200

    feat: remove raspi flake dependency

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11?shallow=1";
     nixos-hardware.url = "github:NixOS/nixos-hardware?shallow=1";
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi?shallow=1";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, nixos-raspberrypi, ... }:
+  outputs = { self, nixpkgs, nixos-hardware, ... }:
   {
     nixosConfigurations = {
-      cyberfish = nixos-raspberrypi.lib.nixosSystemFull {
-        specialArgs = { inherit nixos-raspberrypi; };
+      cyberfish = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit nixos-hardware; };
         system = "aarch64-linux";
         modules = [
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
