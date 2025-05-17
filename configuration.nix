4119daaff186bd8fad4d70e1ec408919901d306d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-17 22:50:08 +0200

    chore: remove builder specific nix config

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,7 @@
 { pkgs, ... }:
 {
-  # Nix settings
+  # Nix state version
   system.stateVersion = "25.05";
-  nix = {
-    optimise.automatic = true;
-    settings = {
-      builders-use-substitutes = true;
-      extra-experimental-features = [ "flakes" "nix-command" ];
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nixos-raspberrypi.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-      ];
-    };
-  };
 
   # Remove documentation to save space
   documentation = {
