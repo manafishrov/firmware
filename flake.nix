ba016bd9abb2b2899fc94f8806176119835680be
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-18 13:14:43 +0200

    fix: trusted public key

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
       "https://nixos-raspberrypi.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNOQ5sPI="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
     ];
   };
 
