1f50f27eecba763462556a61cbd656d8c4334cb2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-20 19:36:06 +0200

    test: pi3 installer image

--- flake.nix
+++ flake.nix
@@ -8,24 +8,22 @@
     ];
   };
 
-  inputs.nixos-raspberrypi.url = "github:michaelbrusegard/nixos-raspberrypi/pi3?shallow=1";
+  inputs.nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi?shallow=1";
 
-  outputs = { self, nixos-raspberrypi, ... }:
+  outputs = { nixos-raspberrypi, ... }:
   {
     nixosConfigurations = {
       cyberfish = nixos-raspberrypi.lib.nixosSystem {
         specialArgs = { inherit nixos-raspberrypi; };
         modules = [
           nixos-raspberrypi.nixosModules.raspberry-pi-3.base
-          nixos-raspberrypi.nixosModules.sd-image
-          ./configuration.nix
         ];
       };
     };
     packages = {
-      aarch64-linux.default = self.nixosConfigurations.cyberfish.config.system.build.sdImage;
-      x86_64-linux.default = self.packages.aarch64-linux.default;
-      aarch64-darwin.default = self.packages.aarch64-linux.default;
+      aarch64-linux.default = nixos-raspberrypi.nixosConfigurations.rpi3-installer.config.system.build.sdImage;
+      x86_64-linux.default = nixos-raspberrypi.nixosConfigurations.rpi3-installer.config.system.build.sdImage;
+      aarch64-darwin.default = nixos-raspberrypi.nixosConfigurations.rpi3-installer.config.system.build.sdImage;
     };
   };
 }
