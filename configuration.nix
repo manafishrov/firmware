c8f063717a665788fa6816e09ae568ac981f663a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-05-28 01:23:03 +0200

    chore: set wi-fi settings

--- configuration.nix
+++ configuration.nix
@@ -32,10 +32,15 @@
     };
     firewall.enable = false;
     nftables.enable = false;
+
+    # Enable Wi-Fi (For downloading temporary packages or files, if permanent should instead be included in this configuration)
+    wireless.iwd.enable = true;
+    networking.networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
   };
 
-  # Enable Wi-Fi (For downloading temporary packages or files, if permanent should instead be included in this configuration)
-  networking.networkmanager.enable = true;
 
   # Enable SSH
   services.openssh = {
