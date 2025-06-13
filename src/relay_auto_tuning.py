59220a0c13fb1b813660be7bc2a104e7bebd4353
Mathias Dam <104485760+capsctrl@users.noreply.github.com>
2025-06-13 16:21:50 +0200

    feat: relay auto tuning now also finds parameters for roll

--- src/relay_auto_tuning.py
+++ src/relay_auto_tuning.py
@@ -87,8 +87,10 @@ def relay_auto_tuning():
         curve_values = np.append(curve_values, [current_time, pitch])
 
         time.sleep(0.02)  # Frequency of 50 Hz
-    
 
+    print("Stopping thrusters")
+    thruster_ctrl.run_thrusters([0, 0, 0, 0, 0, 0])
+    
     print("Relay auto-tuning complete - fitting curve to data")
 
     def sine_wave(x, A, f, phi, offset):
@@ -119,5 +121,99 @@ def relay_auto_tuning():
     K_d = 0.075 * K_u * T_u
 
     print(f"Calculated PID parameters for pitch: K_p = {K_p}, K_i = {K_i}, K_d = {K_d}")
+    print("Waiting 5 seconds for pitch to settle")
     print()
+    time.sleep(5) 
+    
+    
 
+
+
+
+
+    print("STARTING ROLL TUNING")
+    print("Checking roll value")
+    imu.update_pitch_roll()
+    roll = imu.current_roll
+    #TODO: Remove this and allow for non-symmetric ROVs
+    if abs(roll) > 10:
+        print(f"Roll is too high to get good tuning: {roll} degrees, please adjust the ROV buoyancy or ballast tanks to make it level") #
+        raise RuntimeError(f"ROV roll is {roll}°, out of tolerance for auto-tuning")
+    else:
+        print(f"Roll is at {roll} degrees, proceeding with tuning")
+
+
+    print("Finding a (relay amplitude) for roll oscillation")
+    a = 0
+    while True:
+        imu.update_pitch_roll()
+        roll = imu.current_roll
+        pitch = imu.current_pitch
+
+        a += 0.002
+        print(f"Current a: {a}")
+
+        if roll > 0:
+            thruster_ctrl.run_thrusters([0, 0, 0, -pitch*K_p*0.5, 0, a])
+        else:
+            thruster_ctrl.run_thrusters([0, 0, 0, -pitch*K_p*0.5, 0, -a]) # [forward, side, up, pitch, yaw, roll]
+
+        if abs(roll) > 30:
+            print(f"Oscillation of 30 degrees achieved with a = {a}")
+            break
+
+    print(f"Starting relay auto-tuning for roll")
+    curve_values = np.array([])
+    for i in range(500): #10 seconds at 50 Hz
+        imu.update_pitch_roll()
+        roll = imu.current_roll
+        pitch = imu.current_pitch
+        current_time = time.time()
+      
+        if roll > 0:
+            thruster_ctrl.run_thrusters([0, 0, 0, -pitch*K_p*0.5, 0, a]) # [forward, side, up, pitch, yaw, roll]
+        else:
+            thruster_ctrl.run_thrusters([0, 0, 0, -pitch*K_p*0.5, 0, -a]) # [forward, side, up, pitch, yaw, roll]
+
+        curve_values = np.append(curve_values, [current_time, pitch])
+
+        time.sleep(0.02)  # Frequency of 50 Hz
+
+    print("Stopping thrusters")
+    thruster_ctrl.run_thrusters([0, 0, 0, 0, 0, 0])
+    
+    print("Relay auto-tuning complete - fitting curve to data")
+
+    print("Relay auto-tuning complete - fitting curve to data")
+
+    def sine_wave(x, A, f, phi, offset):
+        return A * np.sin(2* np.pi * f* x + phi) + offset
+    
+    time_data = curve_values[0::2]
+    pitch_data = curve_values[1::2]
+    time_data -= time_data[0]  # Normalize time to start at 0
+ 
+
+    initial_guess = [(np.max(pitch_data) - np.min(pitch_data))/2, 1/10, 0, np.mean(pitch_data)] 
+    try:
+        params, _ = curve_fit(sine_wave, time_data, pitch_data, p0=initial_guess)
+        A, f, phi, offset = params
+
+        T_u = 1 / f
+        print(f"Period: {T_u} seconds, Amplitude: {A}")
+
+    except Exception as e:
+        print(f"Error during curve fitting: {e}")
+        return
+    
+    print("Doing calculations for auto tuning")
+    K_u = (4 * a) / (np.pi*A)
+
+    K_p = 0.6 * K_u
+    K_i = 1.2 * K_u / T_u
+    K_d = 0.075 * K_u * T_u
+
+    print(f"Calculated PID parameters for roll: K_p = {K_p}, K_i = {K_i}, K_d = {K_d}")
+    print("Waiting 5 seconds for roll to settle")
+    print()
+    time.sleep(5) 
