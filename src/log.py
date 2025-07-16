a2175f7dd80afd2ed9122463279a986d89ef4ada
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-07-16 08:48:08 +0200

    refactor: log.py to not be uneccesarely verbose

--- src/log.py
+++ src/log.py
@@ -6,43 +6,27 @@ def set_log_is_client_connected_status(is_connected: bool) -> None:
     _is_client_connected = is_connected
 
 
-async def log_info(message: str) -> None:
+async def _log_message(level: str, message: str) -> None:
     if _is_client_connected:
         from websocket_server import get_message_queue
 
         await get_message_queue().put(
             {
                 "type": "logMessage",
-                "payload": {"origin": "firmware", "level": "info", "message": message},
+                "payload": {"origin": "firmware", "level": level, "message": message},
             }
         )
     else:
-        print(f"INFO: {message}")
+        print(f"{level.upper()}: {message}")
+
+
+async def log_info(message: str) -> None:
+    await _log_message("info", message)
 
 
 async def log_warn(message: str) -> None:
-    if _is_client_connected:
-        from websocket_server import get_message_queue
-
-        await get_message_queue().put(
-            {
-                "type": "logMessage",
-                "payload": {"origin": "firmware", "level": "info", "message": message},
-            }
-        )
-    else:
-        print(f"INFO: {message}")
+    await _log_message("warn", message)
 
 
 async def log_error(message: str) -> None:
-    if _is_client_connected:
-        from websocket_server import get_message_queue
-
-        await get_message_queue().put(
-            {
-                "type": "logMessage",
-                "payload": {"origin": "firmware", "level": "info", "message": message},
-            }
-        )
-    else:
-        print(f"INFO: {message}")
+    await _log_message("error", message)
