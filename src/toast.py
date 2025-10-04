b09d200c114a5e7672b449ee00c27be95ab77490
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-05 01:31:22 +0200

    chore: change toast to use message type for cancel

--- src/toast.py
+++ src/toast.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Optional, Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from rov_types import Cancel
+    from .websocket.message import Message
 
 import asyncio
 
@@ -19,9 +19,9 @@ async def _toast_message_async(
     toast_type: Optional[Literal["success", "info", "warn", "error", "loading"]],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
-    from websocket_server import get_message_queue
+    from .websocket.server import get_message_queue
 
     await get_message_queue().put(
         {
@@ -42,7 +42,7 @@ def _toast_message(
     toast_type: Optional[Literal["success", "info", "warn", "error", "loading"]],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     if _main_event_loop and _main_event_loop.is_running():
         asyncio.run_coroutine_threadsafe(
@@ -59,7 +59,7 @@ def toast(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, None, message, description, cancel)
 
@@ -68,7 +68,7 @@ def toast_success(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, "success", message, description, cancel)
 
@@ -77,7 +77,7 @@ def toast_info(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, "info", message, description, cancel)
 
@@ -86,7 +86,7 @@ def toast_warn(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, "warn", message, description, cancel)
 
@@ -95,7 +95,7 @@ def toast_error(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, "error", message, description, cancel)
 
@@ -104,6 +104,6 @@ def toast_loading(
     id: Optional[str],
     message: str,
     description: Optional[str],
-    cancel: Optional[Cancel],
+    cancel: Optional[Message],
 ) -> None:
     _toast_message(id, "loading", message, description, cancel)
