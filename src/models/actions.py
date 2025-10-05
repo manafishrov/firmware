6531e96b338c28e5cd989a519c91fdffcf3066f9
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-10-05 13:01:52 +0200

    feat: add all websocket message types

---
+++ src/models/actions.py
@@ -0,0 +1,3 @@
+DirectionVector = tuple[float, float, float, float, float, float, float, float]
+
+CustomAction = str
--- src/websocket/message.py
+++ src/websocket/message.py
@@ -1,43 +1,74 @@
 from enum import Enum
+from typing import Union
 
+from ..models.actions import DirectionVector, CustomAction
 from ..models.log import LogEntry
 from ..models.toast import Toast
-from ..models.config import RovConfig, ThrusterTest, FirmwareVersion
+from ..models.config import (
+    RovConfig,
+    ThrusterTest,
+    FirmwareVersion,
+    RegulatorSuggestions,
+)
 from ..models.rov_telemetry import RovTelemetry
 from ..models.rov_status import RovStatus
 from ..models.base import CamelCaseModel
 
 
 class MessageType(str, Enum):
-    TELEMETRY = "telemetry"
-    STATUS_UPDATE = "statusUpdate"
-    FIRMWARE_VERSION = "firmwareVersion"
-    CONFIG = "config"
+    DIRECTION_VECTOR = "directionVector"
+    GET_CONFIG = "getConfig"
     SET_CONFIG = "setConfig"
+    CONFIG = "config"
+    START_THRUSTER_TEST = "startThrusterTest"
+    CANCEL_THRUSTER_TEST = "cancelThrusterTest"
+    START_REGULATOR_AUTO_TUNING = "startRegulatorAutoTuning"
+    CANCEL_REGULATOR_AUTO_TUNING = "cancelRegulatorAutoTuning"
+    REGULATOR_SUGGESTIONS = "regulatorSuggestions"
     SHOW_TOAST = "showToast"
     LOG_MESSAGE = "logMessage"
-    CANCEL_REGULATOR_AUTO_TUNING = "cancelRegulatorAutoTuning"
-    CANCEL_THRUSTER_TEST = "cancelThrusterTest"
+    STATUS_UPDATE = "statusUpdate"
+    TELEMETRY = "telemetry"
+    FIRMWARE_VERSION = "firmwareVersion"
+    CUSTOM_ACTION = "customAction"
+    TOGGLE_PITCH_STABILIZATION = "togglePitchStabilization"
+    TOGGLE_ROLL_STABILIZATION = "toggleRollStabilization"
+    TOGGLE_DEPTH_STABILIZATION = "toggleDepthStabilization"
+    FLASH_MICROCONTROLLER_FIRMWARE = "flashMicrocontrollerFirmware"
 
 
-class Telemetry(CamelCaseModel):
-    type: MessageType = MessageType.TELEMETRY
-    payload: RovTelemetry
+class DirectionVector(CamelCaseModel):
+    type: MessageType = MessageType.DIRECTION_VECTOR
+    payload: DirectionVector
 
 
-class StatusUpdate(CamelCaseModel):
-    type: MessageType = MessageType.STATUS_UPDATE
-    payload: RovStatus
+class GetConfig(CamelCaseModel):
+    type: MessageType = MessageType.GET_CONFIG
 
 
-class FirmwareVersion(CamelCaseModel):
-    type: MessageType = MessageType.FIRMWARE_VERSION
-    payload: FirmwareVersion
+class SetConfig(CamelCaseModel):
+    type: MessageType = MessageType.SET_CONFIG
+    payload: RovConfig
 
 
-class ConfigMessage(CamelCaseModel):
+class Config(CamelCaseModel):
     type: MessageType = MessageType.CONFIG
     payload: RovConfig
+
+
+class StartThrusterTest(CamelCaseModel):
+    type: MessageType = MessageType.START_THRUSTER_TEST
+    payload: ThrusterTest
+
+
+class CancelThrusterTest(CamelCaseModel):
+    type: MessageType = MessageType.CANCEL_THRUSTER_TEST
+    payload: ThrusterTest
+
+
+class RegulatorSuggestions(CamelCaseModel):
+    type: MessageType = MessageType.REGULATOR_SUGGESTIONS
+    payload: RegulatorSuggestions
 
 
 class ShowToast(CamelCaseModel):
@@ -50,10 +81,58 @@ class LogMessage(CamelCaseModel):
     payload: LogEntry
 
 
-class CancelRegulatorAutoTuning(CamelCaseModel):
-    type: MessageType = MessageType.CANCEL_REGULATOR_AUTO_TUNING
+class StatusUpdate(CamelCaseModel):
+    type: MessageType = MessageType.STATUS_UPDATE
+    payload: RovStatus
 
 
-class CancelThrusterTest(CamelCaseModel):
-    type: MessageType = MessageType.CANCEL_THRUSTER_TEST
-    payload: ThrusterTest
+class Telemetry(CamelCaseModel):
+    type: MessageType = MessageType.TELEMETRY
+    payload: RovTelemetry
+
+
+class FirmwareVersion(CamelCaseModel):
+    type: MessageType = MessageType.FIRMWARE_VERSION
+    payload: FirmwareVersion
+
+
+class CustomAction(CamelCaseModel):
+    type: MessageType = MessageType.CUSTOM_ACTION
+    payload: CustomAction
+
+
+class TogglePitchStabilization(CamelCaseModel):
+    type: MessageType = MessageType.TOGGLE_PITCH_STABILIZATION
+
+
+class ToggleRollStabilization(CamelCaseModel):
+    type: MessageType = MessageType.TOGGLE_ROLL_STABILIZATION
+
+
+class ToggleDepthStabilization(CamelCaseModel):
+    type: MessageType = MessageType.TOGGLE_DEPTH_STABILIZATION
+
+
+class FlashMicrocontrollerFirmware(CamelCaseModel):
+    type: MessageType = MessageType.FLASH_MICROCONTROLLER_FIRMWARE
+
+
+WebsocketMessage = Union[
+    DirectionVector,
+    GetConfig,
+    SetConfig,
+    Config,
+    StartThrusterTest,
+    CancelThrusterTest,
+    RegulatorSuggestions,
+    ShowToast,
+    LogMessage,
+    StatusUpdate,
+    Telemetry,
+    FirmwareVersion,
+    CustomAction,
+    TogglePitchStabilization,
+    ToggleRollStabilization,
+    ToggleDepthStabilization,
+    FlashMicrocontrollerFirmware,
+]
