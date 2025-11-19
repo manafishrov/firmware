fix: add conversion from degrees to radians before applying PID regulation

src/regulator.py
@@ -234,9 +234,9 @@ class Regulator:
             self.integral_value_pitch += (desired_pitch - current_pitch) * self.delta_t
             self.integral_value_pitch = np.clip(self.integral_value_pitch, -100, 100)
             actuation = (
-                config.pitch.kp * (desired_pitch - current_pitch)
-                + config.pitch.ki * self.integral_value_pitch
-                - config.pitch.kd * -cast(float, self.gyro[1])
+                config.pitch.kp * np.radians(desired_pitch - current_pitch)
+                + config.pitch.ki * np.radians(self.integral_value_pitch)
+                - config.pitch.kd * np.radians(-cast(float, self.gyro[1]))
             )
             current_roll = self.state.regulator.roll
             if current_roll >= PITCH_MAX or current_roll <= PITCH_MIN:
@@ -252,10 +252,11 @@ class Regulator:
             current_roll = self.state.regulator.roll
             self.integral_value_roll += (desired_roll - current_roll) * self.delta_t
             self.integral_value_roll = np.clip(self.integral_value_roll, -100, 100)
+
             actuation = (
-                config.roll.kp * (desired_roll - current_roll)
-                + config.roll.ki * self.integral_value_roll
-                - config.roll.kd * cast(float, self.gyro[0])
+                config.roll.kp * np.radians(desired_roll - current_roll)
+                + config.roll.ki * np.radians(self.integral_value_roll)
+                - config.roll.kd * np.radians(cast(float, self.gyro[0]))
             )
         return actuation
 