f6622dc9795449afff7e2d3540e71ec3f951e778
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-09-07 01:22:19 -0700

    chore(release): bundle auto-zero firmware in v1.1.6-rc.10

--- flake.nix
+++ flake.nix
@@ -40,15 +40,15 @@
       flake = false;
     };
     mcu-firmware-pico = {
-      url = "https://github.com/manafishrov/mcu-firmware/releases/download/v1.0.3-rc.6/pico-v1.0.3-rc.6.uf2";
+      url = "https://github.com/manafishrov/mcu-firmware/releases/download/v1.0.3-rc.7/pico-v1.0.3-rc.7.uf2";
       flake = false;
     };
     mcu-firmware-pico2 = {
-      url = "https://github.com/manafishrov/mcu-firmware/releases/download/v1.0.3-rc.6/pico2-v1.0.3-rc.6.uf2";
+      url = "https://github.com/manafishrov/mcu-firmware/releases/download/v1.0.3-rc.7/pico2-v1.0.3-rc.7.uf2";
       flake = false;
     };
     esc-firmware = {
-      url = "https://github.com/manafishrov/AM32/releases/download/v2.21.0-rc.2/AM32_SKYSTARS_AM60_V2_F421_2.21.0.bin";
+      url = "https://github.com/manafishrov/AM32/releases/download/v2.21.0-rc.3/AM32_SKYSTARS_AM60_V2_F421_2.21.0.bin";
       flake = false;
     };
   };
